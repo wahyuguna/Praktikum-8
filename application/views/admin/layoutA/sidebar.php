cf8c741cc8fa8c376e97db826fe89e23ea66098e
wahyu <wyvlog@gmail.com>
2019-04-27 18:03:49 +0800

    p

--- application/views/admin/layoutA/sidebar.php
+++ application/views/admin/layoutA/sidebar.php
@@ -12,7 +12,7 @@
       </div>
       <!-- sidebar menu: : style can be found in sidebar.less -->
       <ul class="sidebar-menu" data-widget="tree">
-        <li class="treeview">
+        <li class="active">
           <a href="#">
             <i class="fa fa-dashboard"></i> <span>Peminjaman</span>
             <span class="pull-right-container">
